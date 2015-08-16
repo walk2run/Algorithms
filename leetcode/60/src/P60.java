a05ae343aa984a348efef95736a520bd0aeee5f6
qwordy <iamyfy@163.com>
2015-08-16 22:26:35 +0800

    qwordy

--- leetcode/60/src/P60.java
+++ leetcode/60/src/P60.java
@@ -3,11 +3,37 @@
  * Permutation Sequence
  */
 
-public class Solution {
+class Solution {
 	public String getPermutation(int n, int k) {
+		int i, rest, count, j, c;
+		int[] cs = new int[n + 1];
+		cs[0] = 1;
+		for (i = 1; i <= n; i++)
+			cs[i] = cs[i - 1] * i;
 
+		StringBuilder st = new StringBuilder();
+		boolean[] use = new boolean[n + 1];
+		rest = k - 1;
+		for (i = 0; i < n; i++) {
+			count = rest / cs[n - i - 1] + 1;
+			c = 0;
+			for (j = 1; j <= n; j++) {
+				if (!use[j])
+					if (++c == count) {
+						use[j] = true;
+						st.append((char) (j + '0'));
+						break;
+					}
+			}
+			rest = rest % cs[n - i - 1];
+		}
+		return st.toString();
 	}
 }
 
 public class P60 {
+	public static void main(String[] args) {
+		System.out.println(new Solution().getPermutation(2, 2));
+	}
+
 }
---
+++ leetcode/61/src/P61.java
@@ -0,0 +1,23 @@
+/**
+ * Created by yfy on 2015/8/16.
+ * Rotate List
+ */
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
+
+class Solution {
+	public ListNode rotateRight(ListNode head, int k) {
+		ListNode p = head;
+
+	}
+}
+
+public class P61 {
+}
