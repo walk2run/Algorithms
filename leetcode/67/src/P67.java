43e4059dcefe9ad79483eb641982b8c470118bb3
qwordy <iamyfy@163.com>
2015-09-26 16:53:22 +0800

    qwordy

--- leetcode/67/src/P67.java
+++ leetcode/67/src/P67.java
@@ -1,3 +1,6 @@
+import java.math.BigInteger;
+import java.util.Arrays;
+
 /**
  * Created by yfy on 9/22/15.
  * Add Binary
@@ -5,12 +8,28 @@
 
 class Solution {
 	public String addBinary(String a, String b) {
-		int x, y;
-		x = Integer.parseInt(a, 2);
-		y = Integer.parseInt(a, 2);
+		int i, s;
+		char[] c = new char[Math.max(a.length(), b.length()) + 1];
+		Arrays.fill(c, '0');
+		for (i = 0; i < c.length - 1; i++) {
+			s = getNum(a, i) + getNum(b, i) + c[c.length - i - 1] - '0';
+			c[c.length - i - 1] = (char)(s % 2 + '0');
+			c[c.length - i - 2] = (char)(s / 2 + '0');
+		}
+		if (c[0] == '1')
+			return new String(c, 0, c.length);
+		else
+			return new String(c, 1, c.length - 1);
+	}
 
+	int getNum(String a, int i) {
+		if (i < a.length())
+			return a.charAt(a.length() - i - 1) - '0';
+		else
+			return 0;
 	}
 }
 
 public class P67 {
+
 }
---
+++ leetcode/68/src/P68.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+/**
+ * Created by yfy on 9/26/15.
+ * Text Justification
+ */
+
+class Solution {
+	public List<String> fullJustify(String[] words, int maxWidth) {
+
+	}
+}
+
+public class P68 {
+}
