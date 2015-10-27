bff5ae1047e8a921f1138fa18121d95a3ad27822
qwordy <iamyfy@163.com>
2015-10-27 16:58:59 +0800

    qwordy

--- leetcode/72/src/P72.java
+++ leetcode/72/src/P72.java
@@ -5,7 +5,30 @@
 
 class Solution {
 	public int minDistance(String word1, String word2) {
-
+		int i, j, n, m;
+		n = word1.length();
+		m = word2.length();
+		if (n == 0) return m;
+		if (m == 0) return n;
+		int d[][] = new int[n][m];
+		d[0][0] = word1.charAt(0) == word2.charAt(0) ? 0 : 1;
+		for (i = 1; i < m; i++)
+			if (word1.charAt(0) == word2.charAt(i))
+				d[0][i] = i;
+			else
+				d[0][i] = d[0][i - 1] + 1;
+		for (i = 1; i < n; i++)
+			if (word1.charAt(i) == word2.charAt(0))
+				d[i][0] = i;
+			else
+				d[i][0] = d[i - 1][0] + 1;
+		for (i = 1; i < n; i++)
+			for (j = 1; j < m; j++)
+				if (word1.charAt(i) == word2.charAt(j))
+					d[i][j] = d[i - 1][j - 1];
+				else
+					d[i][j] = Math.min(Math.min(d[i - 1][j], d[i][j - 1]), d[i - 1][j - 1]) + 1;
+		return d[n - 1][m - 1];
 	}
 }
 
