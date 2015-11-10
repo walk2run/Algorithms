899bae9b65ce7873f23d00511278a722a4b10b2e
qwordy <iamyfy@163.com>
2015-11-10 11:44:27 +0800

    qwordy

--- leetcode/77/src/P77.java
+++ leetcode/77/src/P77.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -5,10 +6,28 @@ import java.util.List;
  * Combinations
  */
 
-public class Solution {
+class Solution {
 	public List<List<Integer>> combine(int n, int k) {
-
+		this.n = n;
+		this.k = k;
+		search(1, 1);
+		return lists;
 	}
+
+	void search(int depth, int begin) {
+		for (int i = begin; i <= n - k + depth; i++) {
+			list.add(i);
+			if (depth == k)
+				lists.add(new ArrayList<Integer>(list));
+			else
+				search(depth + 1, i + 1);
+			list.remove(list.size() - 1);
+		}
+	}
+
+	int n, k;
+	List<Integer> list = new ArrayList<>();
+	List<List<Integer>> lists = new ArrayList<>();
 }
 
 public class P77 {
