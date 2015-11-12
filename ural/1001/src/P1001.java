697eda95a6628a433110746afcec61c93c418c48
qwordy <iamyfy@163.com>
2015-11-12 21:24:10 +0800

    qwordy

---
+++ ural/1001/src/P1001.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ * Created by yfy on 11/12/15.
+ * 1001. Reverse Root
+ Time limit: 2.0 second
+ Memory limit: 64 MB
+ The problem is so easy, that the authors were lazy to write a statement for it!
+ Input
+ The input stream contains a set of integer numbers Ai (0 ≤ Ai ≤ 1018). The numbers are separated by any number of spaces and line breaks. A size of the input stream does not exceed 256 KB.
+ Output
+ For each number Ai from the last one till the first one you should output its square root. Each square root should be printed in a separate line with at least four digits after decimal point.
+ Sample
+ input	output
+
+ 1427  0
+
+ 876652098643267843
+ 5276538
+
+
+
+
+
+ 2297.0716
+ 936297014.1164
+ 0.0000
+ 37.7757
+
+ */
+
+public class P1001 {
+	public static void main(String[] args) {
+		StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+		System.out;
+		
+		PrintWriter a;
+		BufferedWriter b;
+		Scanner s;
+	}
+}
