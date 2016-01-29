a638b9ef92ee5e70f81891ad6469a222dd835887
qwordy <iamyfy@163.com>
2016-01-29 14:42:55 +0800

    qwordy

--- ural/problems/pull.rb
+++ ural/problems/pull.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'thread'
 
 def main
 	url = 'http://acm.timus.ru/problem.aspx?num='
@@ -14,5 +15,54 @@ def main
 	end
 end
 
-main
+#main
+
+$mutex = Mutex.new
+$mutex2 = Mutex.new
+$resource = ConditionVariable.new
+$id = 1000
+$threadCount = 0
+$url = 'http://acm.timus.ru/problem.aspx?num='
+
+def get
+	id = 0
+	$mutex.synchronize do
+		id = $id
+		puts id
+		$id += 1
+	end
+	is = id.to_s
+	filename = is + '.html'
+	if !File.exist? filename
+		#puts 'Getting ' + is
+		content = Net::HTTP.get(URI($url + is))
+		file = File.new(filename, 'w')
+		file.puts content
+		file.close
+	end
+	$mutex2.synchronize do
+		$threadCount -= 1
+		if $threadCount < 200
+			$resource.signal
+		end
+	end
+end
+
+def pullMulti
+	Dir.mkdir 'p'
+	Dir.chdir 'p'
+	threads = []
+	for i in 1000..2073
+		$mutex2.synchronize do
+			$threadCount += 1
+			if $threadCount > 200
+				$resource.wait $mutex2
+			end
+		end
+		threads << Thread.new { get }
+	end
+	threads.each { |t| t.join }
+end
+
+pullMulti
 
