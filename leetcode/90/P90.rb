9172330aa41a3a9f13cb8c5c350f2a5c3c30b495
qwordy <iamyfy@163.com>
2016-02-09 15:41:59 +0800

    qwordy

---
+++ leetcode/90/P90.rb
@@ -0,0 +1,30 @@
+# 90. Subsets II
+# @param {Integer[]} nums
+# @return {Integer[][]}
+def subsets_with_dup(nums)
+	nums.sort!
+	$nums = nums
+	$lists = []
+	$list = []
+	$n = nums.size
+	search(0)
+	return $lists
+end
+
+def search i
+	if i == $n
+		$lists << Array.new($list)
+		return
+	end
+	if i == 0 || $nums[i] == $nums[i - 1] && $use || $nums[i] != $nums[i - 1]
+		$list << $nums[i]
+		$use = true
+		search i + 1
+		$list.pop
+	end
+	$use = false
+	search i + 1
+end
+
+p subsets_with_dup([1, 2, 2])
+
---
+++ leetcode/91/P91.rb
@@ -0,0 +1,8 @@
+# 91. Decode Ways
+# @param {String} s
+# @return {Integer}
+def num_decodings(s)
+	n = s.size
+
+end
+
