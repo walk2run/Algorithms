8f0eb09ab914fd48ecb1ec53b1b49c3d62564f92
qwordy <iamyfy@163.com>
2016-10-28 22:39:06 +0800

    qwordy

--- leetcode/115/src/P115.java
+++ leetcode/115/src/P115.java
@@ -3,8 +3,25 @@
  * 115. Distinct Subsequences
  */
 
-public class Solution {
+class Solution {
   public int numDistinct(String s, String t) {
+    int i, j, slen, tlen;
+    slen = s.length();
+    tlen = t.length();
+    if (tlen == 0) return 1;
+    if (slen == 0) return 0;
+    int d[][] = new int[slen][tlen];
+    //d[i][j] = d[i-1][j] + (if d[i]==d[j])d[i-1][j-1]
+    if (s.charAt(0) == t.charAt(0)) d[0][0] = 1;
+    for (i = 1; i < slen; i++) {
+      d[i][0] = d[i - 1][0];
+      if (s.charAt(i) == t.charAt(0)) d[i][0]++;
+      for (j = 1; j <= Math.min(i, tlen - 1); j++) {
+        d[i][j] = d[i - 1][j];
+        if (s.charAt(i) == t.charAt(j)) d[i][j] += d[i - 1][j - 1];
+      }
+    }
+    return d[slen - 1][tlen - 1];
   }
 }
 
