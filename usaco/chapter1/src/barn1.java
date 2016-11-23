cfbc07972637d9e8784fe16f6ab98223d8dbb3aa
qwordy <iamyfy@163.com>
2016-11-23 23:54:27 +0800

    qwordy

--- usaco/chapter1/src/barn1.java
+++ usaco/chapter1/src/barn1.java
@@ -6,6 +6,7 @@ LANG: JAVA
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -17,22 +18,29 @@ import java.util.Scanner;
  */
 public class barn1 {
   public static void main(String[] args) throws Exception {
-    int m, s, c, i, no, lastNo, sum;
+    int m, s, c, i, sum;
     Scanner scan = new Scanner(new BufferedInputStream(new FileInputStream("barn1.in")));
     m = scan.nextInt();
     s = scan.nextInt();
     c = scan.nextInt();
-    List<Integer> list = new ArrayList<>();
-    lastNo = scan.nextInt();
-    for (i = 0; i < c; i++) {
-      no = scan.nextInt();
-      list.add(no - lastNo - 1);
-      lastNo = no;
+    PrintWriter pw = new PrintWriter("barn1.out");
+    if (c <= m) {
+      pw.println(c);
+      pw.close();
+      return;
     }
-    list.sort((o1, o2) -> o2 - o1);
+    List<Integer> stalls = new ArrayList<>();
+    for (i = 0; i < c; i++)
+      stalls.add(scan.nextInt());
+    stalls.sort(null);
+    List<Integer> intervals = new ArrayList<>();
+    for (i = 1; i < c; i++)
+      intervals.add(stalls.get(i) - stalls.get(i - 1));
+    intervals.sort((o1, o2) -> o2 - o1);
     sum = 0;
-    for (i = 0; i < m - 1; i++) {
-      sum += list.get(i);
-    }
+    for (i = 0; i < m - 1; i++)
+      sum += intervals.get(i);
+    pw.println(stalls.get(c - 1) - stalls.get(0) + 1 - sum);
+    pw.close();
   }
 }
