ed6b15fa4f2da81bbb319794a989712619c1d179
qwordy <iamyfy@163.com>
2016-12-09 18:02:32 +0800

    qwordy

--- usaco/chapter1/src/wormhole.java
+++ usaco/chapter1/src/wormhole.java
@@ -14,11 +14,13 @@ import java.util.Scanner;
 public class wormhole {
   private static int n;
   private static boolean[] use;
+  private static int[] pairs;
 
   public static void main(String[] args) throws Exception {
     Scanner scan = new Scanner(new File("wormhole.in"));
     n = scan.nextInt();
     Point[] ps = new Point[n];
+    pairs = new int[n];
     for (int i = 0; i < n; i++)
       ps[i] = new Point(scan.nextInt(), scan.nextInt());
     use = new boolean[n];
@@ -27,11 +29,20 @@ public class wormhole {
 
   private static void search(int i) {
     while (i < n && use[i]) i++;
-    if (i < n) use[i] = true; else return;
-    int j = i + 1;
-    while (j < n && use[j]) j++;
-    if (j < n) use[j] = true;
-    search(i + 1);
+    if (i >= n) {
+      System.out.println("count");
+      return;
+    }
+    use[i] = true;
+    for (int j = i + 1; j < n; j++) {
+      if (use[j]) continue;
+      pairs[i] = j;
+      pairs[j] = i;
+      use[j] = true;
+      search(i + 1);
+      use[j] = false;
+    }
+    use[i] = false;
   }
 
   private static class Point {
