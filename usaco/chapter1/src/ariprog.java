d6bfd0abea6f032c9302d988e0c09edc1d48da8b
qwordy <iamyfy@163.com>
2016-12-27 21:50:24 +0800

    qwordy

---
+++ usaco/chapter1/src/ariprog.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * Created by yfy on 12/27/16.
+ * Arithmetic Progressions
+ */
+public class ariprog {
+  public static void main(String[] args) throws Exception {
+    int n, m;
+    Scanner scan = new Scanner(new File("ariprog.in"));
+    n = scan.nextInt();
+    m = scan.nextInt();
+    
+  }
+}
