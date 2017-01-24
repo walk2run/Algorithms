ca37e2bd96d1ced8999a0f910a24531574b8df9b
qwordy <iamyfy@163.com>
2017-01-24 23:58:06 +0800

    qwordy

--- leetcode/10/src/P10.java
+++ leetcode/10/src/P10.java
@@ -3,11 +3,41 @@
  * Regular Expression Matching
  */
 
-public class Solution {
+// Recursive version
+class Solution {
+  private String s, p;
+
+  public boolean isMatch(String s, String p) {
+    this.s = s;
+    this.p = p;
+    return match(0, 0);
+  }
+
+  private boolean match(int i, int j) {
+    if (i == s.length() && j == p.length()) return true;
+    if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+      if (match(i, j + 2)) return true;
+      while (i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')) {
+        if (match(i + 1, j + 2)) return true;
+        i++;
+      }
+      return false;
+    }
+    if (i >= s.length() || j >= p.length()) return false;
+    if (p.charAt(j) == '.' || s.charAt(i) == p.charAt(j))
+      return match(i + 1, j + 1);
+    return false;
+  }
+}
+
+class Solution2 {
   public boolean isMatch(String s, String p) {
 
   }
 }
 
 public class P10 {
+  public static void main(String[] args) {
+    System.out.println(new Solution().isMatch("aa", "a*"));
+  }
 }
