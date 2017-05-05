be9930bfff7712eef7898d7eb678f46198da6e88
qwordy <iamyfy@163.com>
2017-05-05 23:15:52 +0800

    qwordy

--- jb/ms17apr/src/P4.java
+++ jb/ms17apr/src/P4.java
@@ -7,12 +7,23 @@ public class P4 {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     int n = scan.nextInt();
-    for (int i = 0; i < n; i++) {
-      int f = scan.nextInt();
-      int in = scan.nextInt();
-      int ip = scan.nextInt();
-      int c = scan.nextInt();
-      
+    int[] fas = new int[n + 1];
+    int[] ins = new int[n + 1];
+    int[] ips = new int[n + 1];
+    int[] costs = new int[n + 1];
+    int[][] children = new int[n + 1][2000];
+    int root = 1;
+    for (int i = 1; i <= n; i++) {
+      fas[i] = scan.nextInt();
+      ins[i] = scan.nextInt();
+      ips[i] = scan.nextInt();
+      costs[i] = scan.nextInt();
+      if (fas[i] == 0) root = i;
     }
+    System.out.println(kill(root));
+  }
+
+  private static int kill(int p) {
+    
   }
 }
