8e52a6742d3d8ca3bf34e5833d657121299ad1a6
qwordy <iamyfy@163.com>
2017-05-10 00:12:16 +0800

    qwordy

--- jb/ms17apr/src/P4.java
+++ jb/ms17apr/src/P4.java
@@ -5,9 +5,8 @@ import java.util.Scanner;
  * Created by yfy on 2017/4/14.
  */
 public class P4 {
-  static int n;
-  static int[] fa, in, ip, cost;
-  static int[][] children;
+  private static int n;
+  private static int[] fa, in, ip, cost;
 
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
@@ -24,21 +23,24 @@ public class P4 {
       cost[i] = scan.nextInt();
       if (fa[i] == 0) root = i;
     }
-    System.out.println(kill(root));
+    int kill = kill(root);
+    if (kill >= 99999999) kill = -1; else kill += cost[root];
+    System.out.println(kill);
   }
 
   private static int kill(int node) {
     int info = in[node];
     int[] d = new int[info + 1];  // min cost
-    Arrays.fill(d, Integer.MAX_VALUE);
+    Arrays.fill(d, 99999999);
     d[0] = 0;
     for (int i = 1; i <= n; i++) {
       if (fa[i] == node) {
-        for (int j = info; j >= ip[i]; j--)
-          d[j] = Math.min(d[j], Math.max(0, d[j - ip[i]]) + kill(i) + cost[i]);
+        int kill = kill(i);
+        for (int j = info; j > 0; j--)
+          d[j] = Math.min(d[j], d[Math.max(0, j - ip[i])] + kill + cost[i]);
       }
     }
-    System.out.println(node + " " + d[info]);
+    //System.out.println(node + " " + d[info]);
     return d[info];
   }
 }
