df89f80a6845df63e7fb4cfeb8065b247e8750e7
qwordy <iamyfy@163.com>
2017-05-31 21:14:47 +0800

    qwordy

--- usaco/chapter1/pprime/pprime.cpp
+++ usaco/chapter1/pprime/pprime.cpp
@@ -12,16 +12,65 @@ LANG: C++11
  */
 
 #include <fstream>
+#include <cmath>
 using namespace std;
 
+bool prime(int n) {
+  for (int i = 2; i <= sqrt(n); i++)
+    if (n % i == 0) return false;
+  return true;
+}
+
 int main(int argc, char** argv) {
-  int a, b;
+  int a, b, d1, d2, d3, d4, p;
+  
   ifstream fin("pprime.in");
   fin >> a >> b;
   fin.close();
   
+  ofstream fout("pprime.out");
   
+  // 1
+  for (p = 5; p <= 9; p += 2) {
+    if (p > b) { fout.close(); return 0; }
+    if (p >= a && prime(p)) fout << p << '\n';
+  }
   
+  if (a <= 11 && 11 <= b) fout << 11 << '\n';
+  
+  // 3
+  for (d1 = 1; d1 <= 9; d1 += 2) {
+    for (d2 = 0; d2 <= 9; d2++) {
+      p = d1 * 100 + d2 * 10 + d1;
+      if (p > b) { fout.close(); return 0; }
+      if (p >= a && prime(p)) fout << p << '\n';
+    }
+  }
+  
+  // 5
+  for (d1 = 1; d1 <= 9; d1 += 2) {
+    for (d2 = 0; d2 <= 9; d2++) {
+      for (d3 = 0; d3 <= 9; d3++) {
+        p = d1 * 10000 + d2 * 1000 + d3 * 100 + d2 * 10 + d1;
+        if (p > b) { fout.close(); return 0; }
+        if (p >= a && prime(p)) fout << p << '\n';
+      }
+    }
+  }
+  
+  // 7
+  for (d1 = 1; d1 <= 9; d1 += 2) {
+    for (d2 = 0; d2 <= 9; d2++) {
+      for (d3 = 0; d3 <= 9; d3++) {
+        for (d4 = 0; d4 <= 9; d4++) {
+          p = d1 * 1000000 + d2 * 100000 + d3 * 10000 + d4 * 1000 + d3 * 100 + d2 * 10 + d1;
+          if (p > b) { fout.close(); return 0; }
+          if (p >= a && prime(p)) fout << p << '\n';
+        }
+      }
+    }
+  }
+  
+  fout.close();
   return 0;
 }
-
---
+++ usaco/chapter1/sprime/sprime.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: yfy
+ *
+ * Created on May 31, 2017, 9:09 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+  return 0;
+}
+
