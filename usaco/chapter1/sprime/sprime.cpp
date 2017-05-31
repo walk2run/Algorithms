d796b405ea25db200195d59834221e696ec48812
qwordy <iamyfy@163.com>
2017-05-31 21:15:23 +0800

    qwordy

--- usaco/chapter1/sprime/sprime.cpp
+++ usaco/chapter1/sprime/sprime.cpp
@@ -1,24 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ID: iamyfy1
+PROG: sprime
+LANG: C++11
  */
 
 /* 
- * File:   main.cpp
+ * File:   sprime.cpp
  * Author: yfy
  *
  * Created on May 31, 2017, 9:09 PM
  */
 
-#include <cstdlib>
-
+#include <fstream>
 using namespace std;
 
-/*
- * 
- */
 int main(int argc, char** argv) {
+  int n;
+  ifstream fin("sprime.in");
+  fin >> n;
+  fin.close();
+  
+  
 
   return 0;
 }
