908865ae478c34b2d5afae5cc8536d34120724a0
qwordy <iamyfy@163.com>
2017-06-08 22:44:42 +0800

    qwordy

--- usaco/chapter1/sprime/sprime.cpp
+++ usaco/chapter1/sprime/sprime.cpp
@@ -4,24 +4,36 @@ PROG: sprime
 LANG: C++11
  */
 
-/* 
- * File:   sprime.cpp
- * Author: yfy
- *
- * Created on May 31, 2017, 9:09 PM
- */
-
 #include <fstream>
+#include <cmath>
+
 using namespace std;
 
-int main(int argc, char** argv) {
-  int n;
+int n;
+ofstream fout("sprime.out");
+
+bool prime(int num) {
+  for (int i = 2; i <= sqrt(num); i++)
+    if (num % i == 0) return false;
+  return true;
+}
+
+void dfs(int num, int pos) {
+  if (!prime(num)) return;
+  if (pos == n) {
+    fout << num << '\n';
+    return;
+  }
+  for (int i = 1; i <= 9; i += 2)
+    dfs(num * 10 + i, pos + 1);
+}
+
+int main() {
   ifstream fin("sprime.in");
   fin >> n;
   fin.close();
-  
-  
-
+  for (int i : {2, 3, 5, 7})
+    dfs(i, 1);
+  fout.close();
   return 0;
 }
-
