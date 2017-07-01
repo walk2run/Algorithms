4b15ccebe2bede6b2de44a16c9de0de693360843
qwordy <iamyfy@163.com>
2017-07-01 23:39:46 +0800

    qwordy

--- usaco/chapter2/runround/runround.cpp
+++ usaco/chapter2/runround/runround.cpp
@@ -4,7 +4,44 @@ PROG: runround
 LANG: C++11
  */
 #include <fstream>
+#include <vector>
+#include <cstring>
 using namespace std;
+
+bool rr(int m) {
+  vector<int> ds;
+  while (m) {
+    ds.push_back(m % 10);
+    m /= 10;
+  }
+  bool use[10];
+  memset(use, 0, sizeof(use));
+  for (int d : ds) {
+    if (use[d] || d == 0) return false;
+    use[d] = true;
+  }
+  memset(use, 0, sizeof(use));
+  int len = ds.size();
+  int i = len - 1;
+  use[i] = true;
+  while (true) {
+    i = (i - ds[i]);
+    while (i < 0) i += len;
+    if (use[i]) break;
+    use[i] = true;
+  }
+  if (i == len - 1) {
+    for (int j = 0; j < len; j++)
+      if (!use[j]) return false;
+    return true;
+  }
+  return false;
+}
+
+int solve(int m) {
+  while (!rr(++m));
+  return m;
+}
 
 int main() {
   int m;
@@ -12,6 +49,7 @@ int main() {
   fin >> m;
   fin.close();
   ofstream fout("runround.out");
+  fout << solve(m) << '\n';
   fout.close();
   return 0;
 }
