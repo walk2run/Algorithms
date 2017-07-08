3a1bae8b9e23c67b13f6fb2a4f729562040de466
qwordy <iamyfy@163.com>
2017-07-08 23:57:30 +0800

    hello

--- jb/kickstart17roundA/P1.cpp
+++ jb/kickstart17roundA/P1.cpp
@@ -1,24 +1,25 @@
-#include <stdio.h>
+#include <fstream>
 #include <algorithm>
+using namespace std;
 
-FILE *fin = fopen("A-large-practice.in", "r");
-FILE *fout = fopen("A-large.out", "w");
+ifstream fin("A-large-practice.in");
+ofstream fout("A-large.out");
 
 void solve(int n, int m) {
   unsigned long long sum = 0;
   for (unsigned long long i = 1; i < std::min(n, m); i++)
     sum = (sum + (n - i) * (m - i) % 1000000007 * i) % 1000000007;
-  fprintf(fout, "%u\n", sum);
+  fout << sum << '\n';
 }
 
 int main() {  
   int t, n, m;
-  fscanf(fin, "%d", &t);
+  fin >> t;
   for (int i = 1; i <= t; i++) {
-    fprintf(fout, "Case #%d: ", i);
-    fscanf(fin, "%d %d", &n, &m);
+    fout << "Case #" << i << ": ";
+    fin >> n >> m;
     solve(n, m);
   }
-  fclose(fin);
-  fclose(fout);
+  fin.close();
+  fout.close();
 }
