f4db36285cb05c9cb32ab93a16e0184e068a2430
qwordy <iamyfy@163.com>
2017-08-31 23:37:20 +0800

    qwordy

--- leetcode/137/src/P137.java
+++ leetcode/137/src/P137.java
@@ -2,7 +2,14 @@
 
 class Solution {
   public int singleNumber(int[] nums) {
-
+    int ans = 0;
+    for (int i = 0; i < 32; i++) {
+      int sum = 0;
+      for (int num : nums)
+        sum += num >>> i & 1;
+      ans += sum % 3 << i;
+    }
+    return ans;
   }
 }
 
