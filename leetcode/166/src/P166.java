f083d583e7f6f602e2314cd159341c6659d73542
qwordy <iamyfy@163.com>
2017-09-22 23:02:11 +0800

    qwordy

---
+++ leetcode/166/src/P166.java
@@ -0,0 +1,16 @@
+// 166. Fraction to Recurring Decimal
+
+class Solution {
+  public String fractionToDecimal(int numerator, int denominator) {
+    int r = numerator % denominator;
+    String str = "" + numerator / denominator;
+    if (r == 0) return str;
+    int[] a = new int[10];
+    while () {
+
+    }
+  }
+}
+
+public class P166 {
+}
