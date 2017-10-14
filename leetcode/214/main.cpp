90d98805050f4478cc42f2a2ced14a0d0d155d6c
qwordy <iamyfy@163.com>
2017-10-14 21:30:49 +0800

    qwordy

---
+++ leetcode/214/main.cpp
@@ -0,0 +1,15 @@
+// 214. Shortest Palindrome
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+  string shortestPalindrome(string s) {
+
+  }
+};
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
